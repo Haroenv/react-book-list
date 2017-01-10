f5f564b797d5e7ed77ee3dd62059799bf10f278b
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-01-10 15:55:46 +0100

    renamed some things, made first smart component work (booklist)

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import BookList from '../containers/BookList';
 
 class App extends Component {
   render() {
     return (
-      <div>Hi</div>
+      <BookList />
     );
   }
 }
