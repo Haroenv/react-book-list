ad85ee20997c5b3be79ea54a2531a03f6e8b5e97
Arnaud Weyts <arnaud.weyts@gmail.com>
2017-01-10 12:30:14 +0100

    added reducer

---
+++ src/reducer/reducerBooks.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript, the good parts' },
+    { title: 'Harambe' },
+    { title: 'Memes, the good ones' },
+    { title: 'The Dark Tower' },
+  ];
+ }
